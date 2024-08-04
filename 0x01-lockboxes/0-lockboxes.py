77f910626fbdee38dcb3dc6da5cb45f74bff7c9d
ali-hy <ali.haitham.ymy@gmail.com>
2024-08-04 19:49:17 +0400

    0x01-lockboxes/

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" Module that determines if all the boxes can be opened. """
+
+def canUnlockAll(boxes):
+    """ Method that determines if all the boxes can be opened. """
+    n = len(boxes)
+    opened = [False] * n
+    opened[0] = True
+    keys = [0]
+
+    while keys:
+        current_key = keys.pop()
+        for key in boxes[current_key]:
+            if key < n and not opened[key]:
+                opened[key] = True
+                keys.append(key)
+
+    return all(opened)
---
+++ README.md
@@ -0,0 +1,3 @@
+# Interview Prep
+
+Interview preparation
