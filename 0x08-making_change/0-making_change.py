7928c0aa6e8da33f7710af3d3ff4f1b031b883bb
ali-hy <ali.haitham.ymy@gmail.com>
2024-09-22 11:20:35 +0300

    0-making_change.py

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -3,16 +3,17 @@
 
 
 def makeChange(coins, total):
-  '''Make change dynamic programming problem'''
+    '''Make change dynamic programming problem'''
 
-  if total <= 0:
-      return 0
+    if total <= 0:
+        return 0
 
-  dp = [total + 1] * (total + 1)
-  dp[0] = 0
+    dp = [total + 1] * (total + 1)
+    dp[0] = 0
 
-  for coin in coins:
-      for x in range(coin, total + 1):
-          dp[x] = min(dp[x], dp[x - coin] + 1)
+    for coin in coins:
+        for i in range(coin, total + 1):
+            if coin <= i:
+                dp[i] = min(dp[i], dp[i - coin] + 1)
 
-  return dp[total] if dp[total] <= total else -1
+    return dp[total] if dp[total] <= total else -1
